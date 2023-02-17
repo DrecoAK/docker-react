b5478c983912f77758ec13591bce0eeabd192e6b
DrecoAK <drecoak@gmail.com>
2023-02-17 16:24:23 +0000

    Fixed Testing Logic

--- src/App.test.js
+++ src/App.test.js
@@ -5,11 +5,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-  return false;
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
